5cb28fb01b1ba10acebd7f50b53fa9841a773eca
Jim Toth <jim@meme.coach>
2019-07-29 19:08:43 -0400

    Ejects from expo to ExpoKit

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp073bbc17b2c445fca685cbb3dc76cd87://192.168.1.201:19000";
+
+}
