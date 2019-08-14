fb855c13844c137c22a898623a8afe55a9d3c053
Jim Toth <jim@meme.coach>
2019-08-14 16:27:01 -0400

    Updates app icon and splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font } from 'expo';
+import { AppLoading, Font } from 'expo';
 
 import createAppContainer from './navigation/AppContainer';
 import { fetchWelcomeCompleted } from './db/SettingsRepository';
@@ -43,10 +43,6 @@ export default class App extends React.Component<IAppProps, IAppState> {
   _loadResourcesAsync = async () => {
     Promise.all([
       fetchWelcomeCompleted(),
-      // Asset.loadAsync([
-      //   require('./assets/images/robot-dev.png'),
-      //   require('./assets/images/robot-prod.png'),
-      // ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         // ...Ionicons.font,
